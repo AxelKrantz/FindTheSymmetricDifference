ca86880347c6d34d491bff52115db47f62b07ab0
Axel Krantz <axel.krantz@gmail.com>
2021-10-22 14:18:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FindTheSymmetricDifference;
+using System.Collections.Generic;
+
+int[] array1 = new int[] {0, 1, 2, 3};
+int[] array2 = new int[] { 2, 3, 4, 4, 4, 5};
+int[] array3 = new int[] { 4, 32, 4, 1, 7, 6, 5 , 58, 57};
+int[] array4 = new int[] { -3, 5, 4, 2, 12, 1, 2, 3 };
+int[] array5 = new int[] { 5, 4, -6, 3, 7, 6, 3, 2, 1 , 433, 57};
+SymmetricDifference sym = new();
+List<int[]> theListOfArrays = new();
+theListOfArrays.Add(array1);
+theListOfArrays.Add(array2);
+theListOfArrays.Add(array3);
+theListOfArrays.Add(array4);
+theListOfArrays.Add(array5);
+
+int[] result = sym.SymmetricDifferenceMethod(theListOfArrays);
+foreach (int item in result)
+{
+    Console.WriteLine(item);
+}
+
+
+
+
---
+++ SymmetricDifference.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheSymmetricDifference
+{
+    class SymmetricDifference
+    {
+        public int[] SymmetricDifferenceMethod (List<int[]> inputList)
+        {
+            inputList = MakeArraysInListDistinct(inputList);
+            List<int> longList = ConvertListOfArrayToList(inputList);
+            int[] result = RemoveDuplicates(longList);
+            return result;
+        }
+        public List<int[]> MakeArraysInListDistinct(List<int[]> inputList)
+        {
+            List<int[]> distinctList = new();
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                distinctList.Add(inputList[i].Select(x => x).Distinct().ToArray());
+            }
+            return distinctList;
+        }
+        public List<int> ConvertListOfArrayToList(List<int[]> inputList)
+        {
+            List<int> tempList = new();
+            foreach (int[] item in inputList)
+            {
+                tempList.AddRange(item);
+            }
+            return tempList;
+        }
+        public int[] RemoveDuplicates(List<int> inputList)
+        {
+            inputList = inputList.GroupBy(x => x)
+                .Where(g => g.Count() == 1)
+                .Select(y => y.Key)
+                .ToList();
+            return inputList.ToArray();
+        }
+    }
+}
+
+//inputList.Select(x => x).Distinct()
